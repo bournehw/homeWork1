f5bb080eb9e88591a83b5c2b68589ab58b67b2bc
Harrison Bourne <bournehw@muohio.edu>
2012-08-25 16:50:39 -0400

    Added Random Color Fading

--- src/homeWork1App.cpp
+++ src/homeWork1App.cpp
@@ -9,6 +9,9 @@
 
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+#include "cinder/Rand.h"
+#include "Resources.h"
+#include <cmath>
 
 using namespace ci;
 using namespace ci::app;
@@ -20,10 +23,27 @@ class homeWork1App : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+
+	private:
+	float red_;
+	float green_;
+	float blue_;
+	float fadeRed_;
+	float fadeGreen_;
+	float fadeBlue_;
+	int frameCount_;
 };
+
 
 void homeWork1App::setup()
 {
+	red_ = 0;
+	green_ = 0;
+	blue_ = 0;
+	fadeRed_ = 0;
+	fadeGreen_ = 0;
+	fadeBlue_ = 0;
+	frameCount_ = 0;
 }
 
 void homeWork1App::mouseDown( MouseEvent event )
@@ -31,13 +51,36 @@ void homeWork1App::mouseDown( MouseEvent event )
 }
 
 void homeWork1App::update()
-{
+{	if(frameCount_%200==0||frameCount_==0){
+		red_ = Rand::randFloat( 1.0f );
+		//green_ = Rand::randFloat( 1.0f );
+		//blue_ = Rand::randFloat( 1.0f );
+	}
+	if(frameCount_%400==0||frameCount_==0){
+		green_ = Rand::randFloat( 1.0f );
+	}
+	if(frameCount_%600==0||frameCount_==0){
+		blue_ = Rand::randFloat( 1.0f );
+	}
+
+	if(abs(red_-fadeRed_)>.005){
+		fadeRed_ += .005f*((red_-fadeRed_)/abs(red_-fadeRed_));
+	}
+	if(abs(green_-fadeGreen_)>.005){
+		fadeGreen_ += .005f*((green_-fadeGreen_)/abs(green_-fadeGreen_));
+	}
+	if(abs(blue_-fadeBlue_)>.005){
+		fadeBlue_ += .005f*((blue_-fadeBlue_)/abs(blue_-fadeBlue_));
+	}
+
+	frameCount_++;
 }
 
 void homeWork1App::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+
+	gl::clear( Color( fadeRed_, fadeGreen_, fadeBlue_) ); 
+
 }
 
 CINDER_APP_BASIC( homeWork1App, RendererGl )
